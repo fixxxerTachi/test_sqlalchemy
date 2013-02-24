6586d420b790c7f81b225fba471729441bb5afdd
tachi <fixxxer_tachi@yahoo.co.jp>
2013-02-25 07:32:58 +0900

    from sqlalchemy

--- sql.py
+++ sql.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import *
+
 engine=create_engine('mysql://root:pass@localhost/flaskr')
 Base=declarative_base()
